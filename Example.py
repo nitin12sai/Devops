5626c0ff15247f30d97713c9a81c6656c57db4bb
Dasaririshikesh-1403 <124275098+Dasaririshikesh-1403@users.noreply.github.com>
2024-09-22 19:23:05 +0530

    commit

--- Example.py
+++ Example.py
@@ -30,47 +30,19 @@ def xor(a,b):
 def power(a,b):
     return a**b
 
-#divide function @contributor : Patella Nitin Sai
-def power(a,b):
-    return a/b
-#print function function @contributor : Patella Nitin Sai
-
-def divide(a,b):
-    return a/b
-
-# Func in temp branc
-def mul(a, b):
-    return a**b
-
-def temp(a,b,c):
-    return a*b**c
-print(temp(2,3,4))
-
-print(mul(3, 6))
-
 
 #Add function @contributor :R.Arun kumar
 def max_of_four(a, b, c, d):
     return max(a, b, c, d)
 print(max_of_four(1, 5, 3, 4))
 
-## Add function @contributor : J Ruthvik 21BCS047
-def isEven(a):
-    return True if a % 2 == 0 else False
 
-# Added a cool print statement @ contributor : Anil Kumar (21BEC056)
-def cool():
-    print("I Love Devops")
-    print("I love programming")
+#Add function @contributor :D.rishikesh
+def fibonacci(n):
+    sequence = [0, 1]
+    while len(sequence) < n:
+        sequence.append(sequence[-1] + sequence[-2])
+    return sequence
 
-
-## Add function @contributor : MANCHALA SRINU 21BCS062
-def print_lexicographical_numbers(n):
-    numbers = [str(i) for i in range(1, n + 1)]
-    numbers.sort()
-    
-    for number in numbers:
-        print(number)
-
-n = 20  
-print_lexicographical_numbers(n)
+n = int(input("Enter how many Fibonacci numbers you want: "))
+print(fibonacci(n))
